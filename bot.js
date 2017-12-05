d4685cb7deddd31791ebceee12379dcae633210f
unknown <nicolimppu@gmail.com>
2017-12-05 19:05:19 +0200

    Updates

--- bot.js
+++ bot.js
@@ -1,5 +1,4 @@
 var Discord = require('discord.io');
-var Discordjs = require('discord.js');
 var logger = require('winston');
 var auth = require('./auth.json');
 var jsonfile = require('jsonfile');
